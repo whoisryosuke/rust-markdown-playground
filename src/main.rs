4c5d4e7a0f66c08310f621d70c46b89abb324f2f
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-10-02 23:31:21 -0700

    :zap: parse file to ast

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use comrak::{markdown_to_html, ComrakOptions};
 use std::fs;
+extern crate comrak;
+use comrak::nodes::{AstNode, NodeValue};
+use comrak::{format_html, parse_document, Arena, ComrakOptions};
 
 fn main() {
     println!("Opening README \n");
@@ -7,8 +9,48 @@ fn main() {
     let file_path = "README.md".to_string();
     let contents = fs::read_to_string(file_path).expect("Couldn't read file");
 
-    let markdown = markdown_to_html(&contents, &ComrakOptions::default());
+    println!("Markdown parsed into AST \n");
 
-    println!("Markdown parsed into HTML \n");
-    println!("{markdown}");
+    // The returned nodes are created in the supplied Arena, and are bound by its lifetime.
+    let arena = Arena::new();
+
+    // The "root" node, which we parse our markdown into
+    let root = parse_document(&arena, &contents, &ComrakOptions::default());
+
+    // Iterate through the nodes (and their children) recursively
+    // We pass the node to the callback provided as the second function param
+    fn iter_nodes<'a, F>(node: &'a AstNode<'a>, f: &F)
+    where
+        F: Fn(&'a AstNode<'a>),
+    {
+        f(node);
+        for c in node.children() {
+            iter_nodes(c, f);
+        }
+    }
+
+    // Call the iterate nodes function
+    iter_nodes(root, &|node| {
+        // Check the value of the node data
+        match &mut node.data.borrow_mut().value {
+            &mut NodeValue::CodeBlock(ref mut block) => {
+                let orig = std::mem::replace(&mut block.literal, vec![]);
+                println!("Code Block: {}", String::from_utf8(orig).unwrap());
+            }
+            &mut NodeValue::Link(ref mut link) => {
+                let orig = std::mem::replace(&mut link.url, vec![]);
+                println!("Link: {}", String::from_utf8(orig).unwrap());
+            }
+            &mut NodeValue::Strong => {
+                println!("Bold text: ");
+            }
+            // Got text?
+            &mut NodeValue::Text(ref mut text) => {
+                let orig = std::mem::replace(text, vec![]);
+                println!("{}", String::from_utf8(orig).unwrap());
+                // *text = String::from_utf8(orig).unwrap().replace("my", "your").as_bytes().to_vec();
+            }
+            _ => (),
+        }
+    });
 }
